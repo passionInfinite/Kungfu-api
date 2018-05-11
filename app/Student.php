7694a906512a7ae85562457bed03f1a4b425906e
smirk1795 <hardikpatel.hardik36@gmail.com>
2018-05-11 18:53:28 -0400

    Initial Commit

---
+++ app/Student.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Student extends Authenticatable
+{
+    use Notifiable;
+
+    protected $guarded = [];
+
+}
