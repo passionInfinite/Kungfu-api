c27819a35c24f8e6825d0cd8d0ea6184f0701da6
smirk1795 <hardikpatel.hardik36@gmail.com>
2018-05-13 14:55:34 -0400

    Made rank field in the migration as unique.

--- database/migrations/2018_05_11_190543_create_ranks_table.php
+++ database/migrations/2018_05_11_190543_create_ranks_table.php
@@ -16,7 +16,7 @@ class CreateRanksTable extends Migration
         Schema::create('ranks', function (Blueprint $table) {
             $table->increments('id');
             $table->unsignedInteger('level_id');
-            $table->string('belt_color');
+            $table->string('belt_color')->unique();
             $table->foreign('level_id')->references('id')->on('levels');
             $table->timestamps();
         });
